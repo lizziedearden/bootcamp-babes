392e2fd73153741d222787c1ad1ec816af604050
lizziedearden <87985465+lizziedearden@users.noreply.github.com>
2021-09-28 15:00:49 +0100

    font size

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
     return (
         <div className='header-wrapper'>
             <div className="main-info jumbotron">
-            <h1>Bootcamp.io</h1>
+                <h1>Bootcamp.io</h1>
             </div>
         </div>
     )

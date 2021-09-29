1b16d18be3c3376b39104c67b6c7d65326b2a195
coradoran <88389407+coradoran@users.noreply.github.com>
2021-09-29 14:45:17 +0100

    course list changed

--- src/Components/CourseList.js
+++ src/Components/CourseList.js
@@ -1,7 +1,7 @@
 import Course from './Course'
 
 const CourseList = (props) => {
-    const courseComponents = props.course.map((course, index) => {
+    const courseComponents = props.courses.map((course, index) => {
         return <Course course={course} key={index}/>
     });
 

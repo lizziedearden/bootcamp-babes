38fe6b2cc257694c1548f3b2da40ff72bfcc6344
lizziedearden <87985465+lizziedearden@users.noreply.github.com>
2021-09-30 14:10:27 +0100

    adding the delete function

---
+++ src/Containers/DeleteCourse.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class DeleteCourse extends React.Component {
+  state = {
+    id: '',
+  }
+
+  handleChange = event => {
+    this.setState({ id: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios.delete(`http://localhost:8081/api/courses/${this.state.id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Course ID:
+            <input type="text" name="id" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Delete</button>
+        </form>
+      </div>
+    )
+  }
+}

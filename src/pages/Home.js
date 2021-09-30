9d375df1726595423d763e50baf6716d3f1dab51
lizziedearden <87985465+lizziedearden@users.noreply.github.com>
2021-09-30 15:54:13 +0100

    fixed new courses

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,18 +6,45 @@ import Navbar  from "../Components/Navbar";
 import CoursesList from "../Containers/CoursesList"
 import NewCourse from "../Containers/NewCourse"
 import DeleteCourse from '../Containers/DeleteCourse';
+import axios from 'axios';
 
-const Home = () => {
-    return (
-        <div>
-          <Navbar/> 
-          <Header />
-          <Banner title='Course' text='Here are the courses we have to offer...'/>
-          <CoursesList />
-          <NewCourse />
-          <DeleteCourse />
-        </div>
-    )
+
+class Home extends React.Component{
+  constructor (props){
+    super(props);
+    
+    this.state = {
+      courses: []
+    }
+
+    this.getCourses = this.getCourses.bind(this);
+  }
+  
+
+  componentDidMount() {
+    this.getCourses();
+  }
+
+  getCourses(){
+      axios.get(`http://localhost:8081/api/courses`)
+        .then(res => {
+          const courses = res.data;
+          this.setState({ courses });
+        })
+  }
+
+  render() {
+      return (
+          <div>
+            <Navbar/> 
+            <Header />
+            <Banner title='Course' text='Here are the courses we have to offer...'/>
+            <CoursesList courses={this.state.courses}/>
+            <NewCourse getCourses={this.getCourses}/>
+            <DeleteCourse />
+          </div>
+      )
+  }
 }
 
 export default Home

464a33d473587d2c565b6d2972f2196d2f55bf92
Mary1-5 <marymansha15@gmail.com>
2021-10-05 16:18:23 +0100

    css

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react'
 import './Card.css'
 
 function Card(props) {
-    
+
     return (
         <div className="card flip-card" styles="width: 18rem;">
             <div className="flip-card-inner">
                 <div className="card-body flip-card-front">
                     {/* <img src={props.imageSrc} className="card-img-top" alt="..." /> */}
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
-                    <p className="card-deadline">Deadline to apply: {props.deadline}</p>
+                    <div className="centerText">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.text}</p>
+                        <p className="card-deadline">Deadline to apply: {props.deadline}</p>
+                        <p className="card-space_available">Spaces : {props.spacesAvailable}</p>
+                        <p className="card-place">Place : {props.place}</p>
+                    </div>
                 </div>
                 <div className="card-body flip-card-back">
                     <p className="card-description">{props.description}</p>
+                    <p className="card-signUp">Sign-Up at link below: </p>
+                    <a href={props.signUpThrough}>{props.signUpThrough} </a>
+                    
+                    
                     {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">More...</button>
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
